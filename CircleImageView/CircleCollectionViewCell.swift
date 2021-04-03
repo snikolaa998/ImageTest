027fa955d0f91e1376d1969d516c5f03f097519d
Nikola Savic <snikolaa998@gmail.com>
2021-04-03 21:31:33 +0200

    initial commit

---
+++ CircleImageView/CircleCollectionViewCell.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+class CircleCollectionViewCell: UICollectionViewCell {
+    static let identefier = "CircleCollectionViewCell"
+    private let myImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 90 / 2
+        imageView.layer.borderWidth = 1.0
+        imageView.backgroundColor = .blue
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(myImageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        myImageView.frame = contentView.bounds
+    }
+    
+    public func configure(with image: UIImage) {
+        myImageView.image = image
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        myImageView.image = nil
+    }
+    
+}
---
+++ CircleImageView/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  CircleImageView
+//
+//  Created by User on 3.4.21..
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    private var collectionView: UICollectionView!
+    var imageArray: [UIImage] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 90, height: 90)
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView?.register(CircleCollectionViewCell.self, forCellWithReuseIdentifier: CircleCollectionViewCell.identefier)
+        collectionView?.showsHorizontalScrollIndicator = false
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        collectionView?.backgroundColor = .white
+        guard let myCollection = collectionView else {
+            return
+        }
+        view.addSubview(myCollection)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView?.frame = CGRect(x: 0, y: 100, width: view.frame.size.width, height: 90).integral
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CircleCollectionViewCell.identefier, for: indexPath) as! CircleCollectionViewCell
+        cell.configure(with: imageArray[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        imageArray.remove(at: indexPath.row)
+        collectionView.reloadData()
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
+            if imageArray.count == 4 {
+                imageArray[3] = image
+                collectionView.reloadData()
+            } else {
+                imageArray.append(image)
+                collectionView.reloadData()
+            }
+            
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func pickPhotoButton(_ sender: UIButton) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        present(imagePickerController, animated: true)
+    }
+    
+}
+
